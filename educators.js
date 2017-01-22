d11056d9a8f30715c4959ad8d77dd18f261262ad
gabe <guzman.gabriel@outlook.com>
2017-01-22 06:37:00 -0500

    more sockets

--- educators.js
+++ educators.js
@@ -1,6 +1,8 @@
 var code = Date.now().toString;
 code = code.substring(code.length-6, code.length-1);
 
+Document.getElementById('roomCode').innerHTML = code;
+
 var socket = io.connect('45.79.221.136:3000');
 socket.emit('createRoom', code);
 
