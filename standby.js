c3569722569bddaece3fa045188e73194475e4ac
gabe <guzman.gabriel@outlook.com>
2017-01-22 05:05:17 -0500

    student grouping logic for presentation almost done

--- standby.js
+++ standby.js
@@ -1,8 +1,14 @@
 //grab session id from local storage
 var classID = localStorage.getItem("classID");
+var userID = Date.now();
 
+var user {
+	this.classID: classID,
+	this.userID: userID
+}
 //send id to server
 var socket = io.connect('45.79.221.136:3000');
+socket.emit('user', user);
 
 
 //function for what to do when server finishes pairs
