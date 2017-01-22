263712db1b4886cc670af7fe875c3d9c0efdc0eb
gabe <guzman.gabriel@outlook.com>
2017-01-22 02:50:12 -0500

    student sync with fern

--- canvas.js
+++ canvas.js
@@ -1,6 +1,8 @@
 var socketCanvas;
 var canvas;
 
+var mic, recorder, soundFile;
+
 function setup() {
 	canvas = createCanvas(1000,900);
 	background(51);
@@ -8,6 +10,21 @@ function setup() {
 
 	socketCanvas = io.connect('45.79.221.136:3000');
 	socketCanvas.on('mouseCoords', newDrawing);
+
+	// create an audio in
+	mic = new p5.AudioIn();
+
+	// users must manually enable their browser microphone for recording to work properly!
+	mic.start();
+
+	// create a sound recorder
+	recorder = new p5.SoundRecorder();
+
+	// connect the mic to the recorder
+	recorder.setInput(mic);
+
+	// create an empty sound file that we will use to playback the recording
+	soundFile = new p5.SoundFile();
 
 }
 
